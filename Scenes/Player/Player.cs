28749178ecc23897b7acf0e3d11b607835758015
Lucas Falcão <lughfalcao@gmail.com>
2024-12-11 21:39:27 -0300

    merge conflicts

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -72,21 +72,15 @@ public partial class Player : Damageable
 	[Export] private RichTextLabel magLabel;
 	[Export] private RichTextLabel timerLabel;
 	private Timer surviveTimer;
-	[Export] int level = 1;
-	[Export] double experience = 0;
-	[Export] double experienceTotal = 0;
-	[Export] double experienceRequired = 0;
-	[Export] RichTextLabel levelingLabel;
-	[Export] UpgradeMenu upgradeMenu;
 
-    // Slide variables
-    [Export] public float slideSpeed = 60.0f;
-    private bool isSliding = false;
+	// Slide variables
+	[Export] public float slideSpeed = 60.0f;
+	private bool isSliding = false;
 
 	public CollisionShape3D colShape;
 
 
-    public override void _Input(InputEvent @event)
+	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventMouseMotion mouseMotion) HandleCameraRotation(mouseMotion);
 	}
@@ -117,8 +111,8 @@ public partial class Player : Damageable
 
 	public override void _PhysicsProcess(double delta)
 	{
-        float deltaFloat = (float)delta;
-        Vector3 velocityTemp = Velocity;
+		float deltaFloat = (float)delta;
+		Vector3 velocityTemp = Velocity;
 
 		// Gravity
 		if (!IsOnFloor()) velocityTemp += GetGravity() * deltaFloat;
@@ -129,54 +123,54 @@ public partial class Player : Damageable
 		// Reload on Input
 		if (Input.IsActionJustPressed("reload")) HandleReload();
 
-        if (Input.IsActionJustPressed("slide") && Input.IsActionPressed("sprint") && IsOnFloor() && !isSliding)
-        {
-            StartSlide();
-        }
+		if (Input.IsActionJustPressed("slide") && Input.IsActionPressed("sprint") && IsOnFloor() && !isSliding)
+		{
+			StartSlide();
+		}
 
 		if (Input.IsActionJustReleased("slide"))
 		{
 			EndSlide();
 		}
 
-        if (!isSliding)
-        {
-            HandleWalking(deltaFloat, velocityTemp);
-            HandleHeadbob(deltaFloat, velocityTemp);
+		if (!isSliding)
+		{
+			HandleWalking(deltaFloat, velocityTemp);
+			HandleHeadbob(deltaFloat, velocityTemp);
 
-            speed = Input.IsActionPressed("sprint") ? sprintSpeed : walkSpeed;
+			speed = Input.IsActionPressed("sprint") ? sprintSpeed : walkSpeed;
 
 			colShape.Scale = new Vector3(1f, 1f, 1f);
-            colShape.Position = new Vector3(colShape.Position.X, 0f, colShape.Position.Z);
-            cameraController.Position = new Vector3(cameraController.Position.X, 0f, cameraController.Position.Z);
-        }
+			colShape.Position = new Vector3(colShape.Position.X, 0f, colShape.Position.Z);
+			cameraController.Position = new Vector3(cameraController.Position.X, 0f, cameraController.Position.Z);
+		}
 
-        HandleFov(deltaFloat, velocityTemp);
-        HandleShooting();
+		HandleFov(deltaFloat, velocityTemp);
+		HandleShooting();
 		HandleShake(deltaFloat);
 
-        if (isSliding)
-        {
-            velocityTemp += Transform.Basis.Z * slideSpeed;
-            colShape.Scale = new Vector3(0.2f, 0.2f, 0.2f);
+		if (isSliding)
+		{
+			velocityTemp += Transform.Basis.Z * slideSpeed;
+			colShape.Scale = new Vector3(0.2f, 0.2f, 0.2f);
 			colShape.Position = new Vector3(colShape.Position.X, -0.8f, colShape.Position.Z);
-            cameraController.Position = new Vector3(cameraController.Position.X, -0.5f, cameraController.Position.Z);
-        }
+			cameraController.Position = new Vector3(cameraController.Position.X, -0.5f, cameraController.Position.Z);
+		}
 
-        MoveAndSlide();
-        HandleCrosshair(deltaFloat);
-    }
-    private void StartSlide()
-    {
-        isSliding = true;
-    }
+		MoveAndSlide();
+		HandleCrosshair(deltaFloat);
+	}
+	private void StartSlide()
+	{
+		isSliding = true;
+	}
 
-    private void EndSlide()
-    {
-        isSliding = false;
-    }
+	private void EndSlide()
+	{
+		isSliding = false;
+	}
 
-    private static Vector3 GetRandomPointInCircle(Vector3 direction, float radius)
+	private static Vector3 GetRandomPointInCircle(Vector3 direction, float radius)
 	{
 		float angle = (float)GD.RandRange(0, Mathf.Pi * 2);
 
